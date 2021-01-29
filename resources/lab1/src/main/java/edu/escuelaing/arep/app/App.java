1e61b06de9853e8546490ce4e96f099d72b869e8
angiedanielar <angie.ruiz@mail.escuelaing.edu.co>
2021-01-29 16:35:59 -0500

    taller finalizado

---
+++ resources/lab1/src/main/java/edu/escuelaing/arep/app/App.java
@@ -0,0 +1,95 @@
+package edu.escuelaing.arep.app;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
+
+/**
+ * App Class
+ * @author Angie Daniela Ruiz Alfonso
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        LinkedList<Double> myLinkedList = null;
+        LinkedList<Double> myLinkedList2 = null;
+        try {
+        	//test data
+            myLinkedList = makeLinkedList("resources\\data1.txt");
+           //test data
+            myLinkedList2 = makeLinkedList("resources\\data2.txt");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        //test 1
+        double mean = mean(myLinkedList);
+        double standar_deviation = standardDeviation(myLinkedList);
+        System.out.println(String.format("The Mean is %s and the Standard Deviation is %s",mean,standar_deviation));
+       //test 2
+        double mean2 = mean(myLinkedList2);
+        double standar_deviation2 = standardDeviation(myLinkedList2);
+        System.out.println(String.format("The Mean is %s and the Standard Deviation is %s",mean2,standar_deviation2));
+    }
+
+    /**
+     * Creates a linkedlist given its values in an text file
+     * @param testCasePath The path of the file that contain the example values
+     * @return  A LinkedList with the values provided in the laboratory
+     * @throws Exception
+     */
+    public static LinkedList<Double> makeLinkedList(String testCasePath) throws Exception {
+        LinkedList<Double> myLinkedList = new LinkedList<Double>();
+        File file = new File(testCasePath);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        double value;
+        String string;
+        string = bufferedReader.readLine();
+        //while the test path have examples
+        while( string != null){
+            value = Double.parseDouble(string);
+            myLinkedList.add(value);
+            string = bufferedReader.readLine();
+        }
+        return myLinkedList;
+    }
+
+    /**
+     * Calculates the mean of the values stored in an LinkedList
+     * @param linkedList The linkedlist with the values
+     * @return The mean of the values stored in an LinkedList
+     */
+    public static Double mean(LinkedList<Double> linkedList) {
+    	//taken: https://www.programiz.com/java-programming/examples/standard-deviation
+    	
+        double sum = 0.0;
+        for(double value:linkedList){
+            sum = sum+value;
+        }
+        double ans = (sum / linkedList.getSize());
+        
+        //we aproximate to match with the example path values
+        return (double)Math.round(ans * 100d) / 100d;
+    }
+
+    /**
+     * Calculates the standard deviation of the values stored in an LinkedList
+     * @param linkedList The linkedlist with the values
+     * @return The standard deviation of the values stored in an LinkedList
+     */
+    public static double standardDeviation(LinkedList<Double> linkedList){
+        double mean = mean(linkedList);
+        double sum = 0.0;
+        double x;
+        for (double value:linkedList){
+            x=value-mean;
+            sum = sum+x*x;
+        }
+        double ans = Math.sqrt(sum/(linkedList.getSize()-1));
+
+        //we aproximate to match with the example path values
+        return (double)Math.round(ans * 100d) / 100d;
+    }
+}
---
+++ resources/lab1/src/main/java/edu/escuelaing/arep/app/LinkedListIterator.java
@@ -0,0 +1,44 @@
+package edu.escuelaing.arep.app;
+
+import java.util.Iterator;
+
+/**
+ * Iterator for the LinkedList
+ * @author Angie Daniela Ruiz Alfonso
+ * @param <T> Type of elements returned by the iterator object
+ */
+public class LinkedListIterator<T> implements Iterator<T> {
+
+	//in this moment
+    Node<T> current;
+
+    /**
+     * Iterator for an linkedList constructor
+     * @param linkedList The linkedList which is assigned the iterator
+     */
+    public LinkedListIterator(LinkedList<T> linkedList) {
+        current = linkedList.getHead();
+    }
+
+    /**
+     * Returns true if the iteration has more elements or false if not
+     * @return True if the iteration has more elements or False if not
+     */
+    public boolean hasNext() {
+        return current != null;
+    }
+
+    /**
+     * Returns the next element in the iteration.
+     * @return The next element in the iteration
+     */
+    public T next() {
+        T value = current.getValue();
+        current = current.getNext();
+        return value;
+    }
+
+	//does nothing
+    public void remove() {
+    }
+}
